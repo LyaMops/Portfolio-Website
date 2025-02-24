3356ae74dde9e8cb54780a9a3174c442c789de09
Oleksandr <nesterenkooleksandr2005@gmail.com>
2025-02-24 17:52:38 +0100

    Before trying to make adaptability

---
+++ script.js
@@ -0,0 +1,35 @@
+const tabLinks = document.querySelectorAll(".tab-links");
+const tabContents = document.querySelectorAll(".tab-contents");
+
+tabLinks.forEach(button => {
+    button.addEventListener('click', function() {
+        const targetId = this.getAttribute('data-target');
+        removeTabs();
+        this.classList.add('active-link');
+        document.getElementById(targetId).classList.add('active-tab');
+    })
+});
+
+function removeTabs() {
+    tabLinks.forEach(element => {
+        element.classList.remove('active-link');
+    })
+    tabContents.forEach(element => {
+        element.classList.remove('active-tab');
+    })
+}
+
+const openSideMenuBtn = document.querySelector('.fa-solid.fa-bars');
+const closeSideMenuBtn = document.querySelector('.fa-solid.fa-xmark');
+const navList = document.querySelector('nav');
+
+function openSideMenu() {
+    navList.querySelector('ul').classList.add('nav-active');
+}
+
+function closeSideMenu() {
+    navList.querySelector('ul').classList.remove('nav-active');
+}
+
+openSideMenuBtn.addEventListener('click', openSideMenu);
+closeSideMenuBtn.addEventListener('click', closeSideMenu); 
